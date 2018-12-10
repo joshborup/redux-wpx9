9859c0019fb33e14225f10160e3c3051403690ad
joshborup <joshborup@gmail.com>
2018-12-10 00:54:29 -0700

    final commit

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Login from "./Login";
+import UpdateName from "./UpdateName";
 import { loginUser } from "../../ducks/reducer";
 
 class Content extends Component {
 	render() {
-		const { loginUser } = this.props;
+		const { loginUser, user } = this.props;
 		return (
 			<div className="content">
 				<div className="d-c">
-					{this.props.user ? (
-						`${this.props.user} is super cool`
+					{user ? (
+						<div>
+							{`${user} is super cool`}
+							<UpdateName />
+						</div>
 					) : (
 						<Login login={loginUser} />
 					)}
